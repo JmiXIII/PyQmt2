3dc0499010f698ee2d148677604726de1c3f214d
Sylvain Page(s.page@hpo-samat.com) <user11@hpo-samat.fr>
2016-10-21 17:42:05 +0200

    Week groupby
    Aggrégation à la semaine + études différents graphes

--- PyQmt2.py
+++ PyQmt2.py
@@ -26,7 +26,6 @@ def scan(path, atr, key, size):
     return liste
 
 
-# % import
 def file2df(elmnt):
     data = pd.read_csv(elmnt, header=None,
                        sep='\t+',
@@ -36,9 +35,10 @@ def file2df(elmnt):
     data.rename(columns=b, inplace=True)
     data = data.dropna()
     return data
-#
 
-a = scan(path, 'log', '*108898*',None)
+
+# %% chargement des données
+a = scan(path, 'log', '*2016*', None)
 dfs = []
 for file in a:
     try:
@@ -47,25 +47,41 @@ for file in a:
         df.c_5.astype(np.float)
         dfs.append(df)
     except:
-        print("No+ "+file)
+        print("error + "+file)
+
+# %% formatage des données
 data = pd.concat(dfs)
 data.c_6 = data.c_6.astype(np.float)
 data.c_5 = data.c_5.astype(np.float)
+data.c_0 = pd.to_datetime(data.c_0)
+data.c_0 = data.c_0.dt.week
+week = data.c_0.unique()
+week.sort()
+
+# %% préparation des données
 grouped = data.groupby('c_0', as_index=True)
 mean = grouped.c_5.mean().get_values()
 std = grouped.c_5.std().get_values()
 xx = [x for x in range(len(mean))]
 y = [x for x in grouped.size().index]
+
+# %% plot
+plt.style.use('seaborn-paper')
+#plt.rcParams['figure.figsize']=(20,10)
+#plt.rcParams['figure.fontsize']=20
 plt.figure(1)
 g1 = plt.plot(mean)
 plt.fill_between(xx, mean-3*std,
                  mean+3*std, color="#3F5D7D")
 plt.figure(2)
-plt.style.use('seaborn-paper')
 sbn.distplot(data.c_5)
-fig, ax = plt.subplots(1, 1)
-plt.figure(3, figsize=(12, 3))
-g2 = sbn.stripplot(data.c_5, jitter=True)
+# g2 = sbn.stripplot(data.c_5, jitter=True)
+plt.figure(3)
+sbn.tsplot(mean, time=week)
+plt.figure(4)
+sbn.violinplot(data.c_5, groupby=data.c_0)
+plt.figure(5)
+sbn.boxplot(data.c_5, groupby=data.c_0)
 # g2.axes.set_xticks(np.linspace(3.95,4.10,16,endpoint=True))
 # g2.axes.set_xlim(3.95,4.10)
 
